7adb3821c1b03f1a74a246b1258ad00faf151f0a
fabiel016 <fabiel016_@hotmail.com>
2014-10-20 23:47:40 -0500

    generate subclaves

--- src/logica/DES.java
+++ src/logica/DES.java
@@ -11,10 +11,10 @@ package logica;
  */
 public class DES implements CifradoCesar {
     
-    private String palabraLimpias;
-    private String[] claves;
-    private String palabraCifrada;
-    private  String[] alfabeto;
+    private static String palabraLimpias;
+    private static String[][] claves;
+    private static String palabraCifrada;
+    private static  String[] alfabeto;
     
 
     /**
@@ -22,6 +22,8 @@ public class DES implements CifradoCesar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        DES.alfabeto = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        generarClaves(16);
     }
 
     public DES() {
@@ -35,10 +37,25 @@ public class DES implements CifradoCesar {
         
     }
     
-    public void generarClaves(int numClaves){
-        claves = new String[numClaves];
+    /**
+     * @description this metod generate n number of sub pass
+     * @autor Johan fabiel casas diaz <fabiel016_@hotmail.com>
+     * @param numClaves 
+     */
+    public static void generarClaves(int numClaves){
+        claves = new String[numClaves][DES.alfabeto.length];
+        int k = 0;
         for (int i = 0; i < numClaves; i++){
-            
+            for (int j = i; j < DES.alfabeto.length; j ++){
+                claves[i][j] = DES.alfabeto[k];
+                k++;
+            }
+            for (int j = 0; j < i; j ++){
+                claves[i][j] = DES.alfabeto[k];
+                k ++;
+            }
+            k = 0;
+            System.out.println(claves[i]);
         }
     }
     
